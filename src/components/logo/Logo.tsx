e2c47f3b12599de6646eb0ad601bb2359d6b2df0
max <kodtya.oreshin@inbox.ru>
2025-05-08 18:20:30 +0300

    add logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -10,7 +10,7 @@ interface LogoProps {
 const Logo: React.FC<LogoProps> = ({ classname }) => {
   return (
     <Image
-      src="/Images/about/tottal.jpg"
+      src="/Images/Logo.svg"
       alt="Logo"
       width={100}
       height={100} // Убедитесь, что высота соответствует ширине, если логотип квадратный
