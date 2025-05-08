e5d2fd762cb3d25b71011a48dc7ac7dce1fd6267
max <kodtya.oreshin@inbox.ru>
2025-05-08 18:06:06 +0300

    add logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import styles from "./Logo.module.scss";
+// import styles from "./Logo.module.scss";
 
 interface LogoProps {
   classname?: string; // Опциональное свойство width
