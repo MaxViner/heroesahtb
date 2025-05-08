6c19e0a2a2bd8a625f657a5d051e991526137d2d
max <kodtya.oreshin@inbox.ru>
2025-05-08 17:53:11 +0300

    add logo

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+import styles from "./Logo.module.scss";
+
+interface LogoProps {
+  classname?: string; // Опциональное свойство width
+}
+
+// Основной экспорт компонента
+const Logo: React.FC<LogoProps> = ({ classname }) => {
+  // Установка значения по умолчанию
+  return (
+    <Image
+      src="/logo.png"
+      alt="Logo"
+      width={100}
+      height={100}
+      className={`${styles.logo} ${classname}`}
+    />
+  );
+};
+
+export default Logo;
